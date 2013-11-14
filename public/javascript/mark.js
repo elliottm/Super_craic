d3349846c229e5f70ba6e2b2d550845b6bb8d39f
elliottm <m_elliott@live.com>
2013-11-14 18:49:24 +0000

    ahoy ahoy

--- public/javascript/mark.js
+++ public/javascript/mark.js
@@ -10,5 +10,14 @@ function homepage() {
 $.get('/', function(data) {
         $(data).appendTo("#mainpage")
         });
-
 }
+
+function keeydown() {
+
+$('body').on('keydown keyup',function(r){
+      var color = r.type=="keydown" ? 'red' : 'white' ;
+      if(r.which==82){
+          $(this).css({background: color});  
+      }
+    });
+};
